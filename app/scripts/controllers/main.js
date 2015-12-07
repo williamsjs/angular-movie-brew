1217c1d7a9ca696887b48168e2a95addd938c0ff
Scott Williams <williamsjs1224@gmail.com>
2015-12-07 12:57:08 -0500

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name movieBrewApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the movieBrewApp
+ */
+angular.module('movieBrewApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
